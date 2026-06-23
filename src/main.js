4cde488a8721c335a905bcefed160456af4d58be
tauro83 <julian.villegas.alonzo@gmail.com>
2026-06-23 10:36:47 -0500

    navBar+maps

--- src/main.js
+++ src/main.js
@@ -4,13 +4,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import HomeView from './views/HomeView.vue'
 import UsuarioView from './views/UsuarioView.vue'
+import InquilinoView from './views/InquilinoView.vue'
+import ServiciosView from './views/ServiciosView.vue'
+import SucursalView from './views/SucursalView.vue'
 import './assets/main.css'
 
 const router = createRouter({
     history:createWebHistory(),
     routes:[
         {path:'/',component:HomeView},
-        {path:'/Usuarios', component:UsuarioView}
+        {path:'/Usuarios', component:UsuarioView},
+        {path:'/Inquilinos', component:InquilinoView},
+        {path:'/Servicios', component:ServiciosView},
+        {path:'/Sucursales', component:SucursalView},
+
     ]
 
 })
